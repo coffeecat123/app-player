75de71f33d04160c3a648da08f9416959b6b06eb
coffeecat <coffeeccaatt@gmail.com>
2025-11-16 23:01:06 +0800

    add repeat,shuffle,always restart

--- app/src/main/java/com/coffeecat/player/utils/Settings.kt
+++ app/src/main/java/com/coffeecat/player/utils/Settings.kt
@@ -5,5 +5,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Settings(
     val autoPlay: Boolean = true,
-    val backgroundPlaying: Boolean = true
+    val backgroundPlaying: Boolean = true,
+    val alwaysRestart: Boolean = false
 )
